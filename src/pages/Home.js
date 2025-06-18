eb191f142da5bd42cd17eba8c5b7b45300db2a39
Melrick Davison <115827008+MelrickDavison@users.noreply.github.com>
2025-06-17 23:51:59 -0300

    Update styles and create book cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,32 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import '../App.css';
 
 export default function Home(){
     const [livros, setLivros] = useState([])
 
   useEffect( () => {
-      axios.get('http://gutendex.com/books')
+      axios.get('https://gutendex.com/books')
       .then(res => setLivros(res.data.results))
       .catch(err => console.error(err));
   }, []);
 
   return(
   <div>
-      <h1>Catálogo PNLD</h1>
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+      <h1 className="title-top">ESTANTE VIRTUAL</h1>
+      <div className="book-container">
         {livros.map((livro) => {
-          // Pega a URL da capa
           const coverUrl = livro.formats["image/jpeg"];
-
-          // Se tiver capa, mostra
           return (
-            <div key={livro.id} style={{ width: "150px" }}>
-              {coverUrl ? (
-                <img
-                  src={coverUrl}
-                  alt={`Capa do livro ${livro.title}`}
-                  style={{ width: "100%", height: "auto" }}
-                />
-              ) : (
-                <div
-                  style={{
-                    width: "150px",
-                    height: "200px",
-                    backgroundColor: "#ccc",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    fontSize: "12px",
-                    color: "#555",
-                  }}
-                >
-                  Sem capa
+            <div  className="book-card">
+                <div key={livro.id}>
+                    <img
+                    className="img-book"
+                    src={coverUrl}
+                    alt={`Capa do livro ${livro.title}`}
+                    />
+                    <p className="book-info">{livro.title}</p>
                 </div>
-              )}
-              <p>{livro.title}</p>
             </div>
           );
         })}
